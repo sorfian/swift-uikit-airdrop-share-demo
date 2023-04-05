ebac312aa47713e1bc769b0901ace55a6972cacd
Sorfian <sorfian.tkj@gmail.com>
2023-04-05 13:49:48 +0800

    Initial Commit

---
+++ AirDrop Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirDrop Demo App
+//
+//  Created by Sorfian on 05/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
